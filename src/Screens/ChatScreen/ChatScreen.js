ffbdc45a8371cfee2b34a68344d3e58de7748458
MATAN\matan <matany90@gmail.com>
2019-12-08 21:06:39 +0200

    endpoint deployment refactor

--- src/Screens/ChatScreen/ChatScreen.js
+++ src/Screens/ChatScreen/ChatScreen.js
@@ -15,7 +15,7 @@ const ChatScreen = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://chat-app-server-matan.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
