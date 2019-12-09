7b67e2270419f37e0aa5c0674bde7bb6aca9f288
MATAN\matan <matany90@gmail.com>
2019-12-09 11:21:23 +0200

    height refactor main screen

---
+++ src/utils/Translates.js
@@ -0,0 +1,25 @@
+const EN = 'EN';
+const HE = 'HE';
+
+const LANGUAGE = HE;
+
+export default Translates = {
+    MainScreen: {
+        heading: {} ,
+        subHeading: {
+
+        },
+        namePlaceholder: {
+
+        },
+        roomPlaceholder: {
+
+        },
+        button: {
+
+        }
+    },
+    ChatScreen: {
+
+    }
+}
